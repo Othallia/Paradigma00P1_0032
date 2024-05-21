e10cdf100672b4355f813d2a852ae92ad4efd277
Othallia <vianiputriocta6@gmail.com>
2024-05-21 17:39:35 +0700

    membuat public

--- Inisialisasi Pemdas/Inisialisasi Pemdas.cpp
+++ Inisialisasi Pemdas/Inisialisasi Pemdas.cpp
@@ -10,3 +10,8 @@ private:
 	const int id; //3 variabel mahasiswa
 	string nama;
 	float nilai;
+
+public:
+	// inisialisasi akan mmeberikan awal nilai variabel melalui konstruktor
+	// membuat konstruktor yg akan membuat nilai awal ketika program di jalankan
+	mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai) {
